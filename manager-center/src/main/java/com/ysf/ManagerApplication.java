f8f84f5f999ecd69b23d64ee7b2c805b5e624f7f
孙文星 <812733381@qq.com>
2017-12-31 23:26:02 +0800

    增加mybatis-plus配置

--- manager-center/src/main/java/com/ysf/ManagerApplication.java
+++ manager-center/src/main/java/com/ysf/ManagerApplication.java
@@ -1,12 +1,16 @@
 package com.ysf;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ManagerApplication {
+	private static Logger log = LoggerFactory.getLogger(ManagerApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
+		log.debug("系统启动成功");
 	}
 }
