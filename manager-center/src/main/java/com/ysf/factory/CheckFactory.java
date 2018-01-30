6975f9b08521d957a612c9f812a0bc6d38348229
孙文星 <812733381@qq.com>
2018-01-30 17:48:45 +0800

    修改工厂方法

--- manager-center/src/main/java/com/ysf/factory/CheckFactory.java
+++ manager-center/src/main/java/com/ysf/factory/CheckFactory.java
@@ -1,6 +1,8 @@
 package com.ysf.factory;
 
 import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 import com.ysf.common.bo.UserInfo;
@@ -12,6 +14,8 @@ import com.ysf.util.ShiroUtil;
 @Component
 @DependsOn("springContextHolder")
 public class CheckFactory implements ICheckFactory {
+	private static final Logger log = LoggerFactory.getLogger(CheckFactory.class);
+	
 	/**
 	 * 检查指定角色
 	 */
@@ -21,7 +25,7 @@ public class CheckFactory implements ICheckFactory {
 		if (null == userInfo) {
 			return false;
 		}
-		String join = CollectionUtil.join(permissions, ",");
+		String join = CollectionUtil.join(permissions,",");
 		if (ShiroUtil.hasAnyRoles(join)) {
 			return true;
 		}
@@ -39,11 +43,19 @@ public class CheckFactory implements ICheckFactory {
 			return false;
 		}
 		String requestURI = request.getRequestURI().replaceFirst(ConfigListener.getConf().get("contextPath"),"");
+		
+		log.debug("检查全体角色请求路径:" + requestURI);
+		
 		String[] str = requestURI.split("/");
+		
+		log.debug(str.toString());
+		
 		if (str.length > 3) {
 			requestURI = "/" + str[1] + "/" + str[2];
 		}
-		System.out.println(requestURI);
+	
+		log.debug("检查全体角色请求路径:" + requestURI);
+		
 		if (ShiroUtil.hasPermission(requestURI)) {
 			return true;
 		}
